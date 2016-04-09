e48643e537cbf1f45650640471a96591b421b495
Svetlana Morozko <morozko.svetlana@gmail.com>
2016-04-09 14:23:40 +0300

    Task block 03 is done

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return Date.parse(value);
 }
 
 
@@ -56,7 +56,11 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   var year = date.getFullYear();
+   return year%4 == 0 
+       && year%100 !== 0
+       || year%100 == 0
+       && year%400 == 0;
 }
 
 
@@ -76,9 +80,11 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    var timeSpan = new Date(endDate-startDate);
+    timeSpan = timeSpan.toISOString();
+    return timeSpan.slice(11, -1);
 }
-
+//console.log(timeSpanToString(new Date(2000,1,1,10,0,0), new Date(2000,1,1,10,30,0)))
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
@@ -94,7 +100,18 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    var newDate = new Date(date);
+    var hours = newDate.getUTCHours();
+    var minutes = newDate.getUTCMinutes();
+    var angleDeg, angleRad;
+    var hr = Math.abs(hours-Math.round(hours/12)*12);
+    var min = Math.abs(minutes-Math.round(minutes/60)*60);
+    if (minutes <= 30){
+        angleDeg = Math.abs((hr*60+minutes)*0.5-min*6);
+    } else{
+        angleDeg = Math.abs((hr*60-minutes)*0.5-min*6);
+    }
+    return angleRad = angleDeg*Math.PI/180;
 }
 
 
